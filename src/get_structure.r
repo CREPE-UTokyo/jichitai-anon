7860d8062155b05b3f3827d755727e51acaa3e55
watagori <kotaro.nishigori@gmail.com>
2023-07-22 02:26:47 +0900

    feat: get random and plot

--- src/get_structure.r
+++ src/get_structure.r
@@ -1,73 +1,155 @@
+# パッケージをインストールする関数
+# install_packages <- function(package_name) {
+#   if (!require(package_name, character.only = TRUE)) {
+#     install.packages(package_name, lib = "/my_packages", repos = "http://cran.us.r-project.org")
+#     library(package_name, character.only = TRUE)
+#   }
+# }
+
+# # Check and install necessary packages
+# necessary_packages <- c("purrr")
+
+# sapply(necessary_packages, install_packages)
 .libPaths(c("/my_packages", .libPaths()))
 library(readr)
+library(dplyr)
+library(showtext)
+library(purrr)
+showtext_auto()
+font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
 
 
-# 必要となるファイル一覧を取得
-.get_file_list <- function(directory, pattern) {
-  files <- list.files(path = directory, pattern = pattern, full.names = TRUE)
-  if (length(files) == 0) {
-    stop(paste("次のパターンのファイルが存在していません'", pattern, "' ファイル名が間違っていないか確認してください。", sep = ""))
-  }
-  return(files)
+.get_setting <- function() {
+  data <- read_csv("config/setting_plot.csv")
+  column_list <- lapply(data, as.character)
+  return(column_list)
 }
 
-# TODO:エンコーディングがことなってくる場合の対処
-# ヘッダーを取得
-.check_header <- function(file_list, headers) {
-    for (file in files) {
-    # ファイルを読み込む
-    data <- read_csv(file, locale = locale(encoding = "shift-jis"), n_max = 0)
-    # ヘッダーに"年度"が含まれていない場合、エラーを出す
-    for (header in headers) {
-        if (!header %in% names(data)) {
-            stop(paste("エラー: ファイル", file, "には '", header, "' という単語がヘッダーに含まれていません。"))
-        }
+.get_files <- function(filename) {
+  file_list <- list.files(path = "data", pattern = paste0("^", filename, ".*\\.csv$"), full.names = TRUE)
+}
+
+.save_structure <- function(file, header) {
+    encoding_list <- c("UTF-8", "Shift-JIS")
+    
+    for (encoding in encoding_list) {
+        result <- tryCatch({
+            # Try to read the file with the current encoding
+            data <- read_csv(file, locale = locale(encoding = encoding))
+            
+            # Proceed if no error
+            distribution <- table(data[[header]])
+            filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
+            filename_saved <- paste0("output/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
+            png(filename = filename_saved, family = "IPAex")  # ここでフォントを指定
+            barplot(distribution, 
+                    xlab = filename, 
+                    ylab = "Frequency", 
+                    col = rainbow(length(distribution)))
+            dev.off()
+            
+            TRUE  # return TRUE if everything went well
+        },
+        error = function(e) {
+            FALSE  # return FALSE if there was an error
+        })
+        
+        # If there was no error, exit the loop
+        if (result) break
     }
-    print(paste("ヘッダーの読み取り完了", file))
+    
+    # If none of the encodings worked, guess the encoding
+    if (!result) {
+        guessed_encoding <- .guess_file_encofing(file)
+        
+        # Try to read the file with the guessed encoding
+        data <- read_csv(file, locale = locale(encoding = guessed_encoding))
+        
+        # Proceed if no error
+        distribution <- table(data[[header]])
+        filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
+        filename_saved <- paste0("output/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
+        png(filename = filename_saved, family = "IPAex")  # ここでフォントを指定
+        barplot(distribution, 
+                xlab = filename, 
+                ylab = "Frequency", 
+                col = rainbow(length(distribution)))
+        dev.off()
     }
 }
 
 
-# データ概要報告で必要となるヘッダーを取得
-.get_config_headers <- function() {
-    settings <- readr::read_csv("config/setting.csv", locale = locale(encoding = "utf-8"))
-    headers <- settings$headers
-    if (is.null(headers) || length(headers) == 0) {
-        stop("No headers found in the settings file.")
+
+# 精度が低いので使いたくない
+.guess_file_encofing <- function(filename) {
+    # ファイルのエンコーディングを推測
+    guess <- guess_encoding(filename, n_max = 1000)
+    # エンコーディングを返す
+    return(guess$encoding[1])  # encodingの最初の推測を返す
+}
+
+
+.save_random <- function(filename, header) {
+    # Check the file encoding and read the file
+    encoding_list <- c("UTF-8", "Shift-JIS")
+    for (encoding in encoding_list) {
+        result <- tryCatch({
+            # Try to read the file with the current encoding
+            data <- read_csv(filename, locale = locale(encoding = encoding))
+            TRUE  # return TRUE if everything went well
+        },
+        error = function(e) {
+            FALSE  # return FALSE if there was an error
+        })
+
+        # If there was no error, exit the loop
+        if (result) break
     }
-    return(headers)
-}
-
-
-.save_plot <- function(file, header) {
-    # CSVファイルを読み込む
-    data <- read_csv(file, locale = locale(encoding = "shift-jis"))
-    # 指定されたヘッダーの分布を計算
-    distribution <- table(data[[header]])
-    # 分布を表示
-    print(distribution)
-    # ファイル名を生成
-    file_name <- paste0("output/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
-    # 画像の保存先とファイル名を指定
-    png(filename = file_name)
-    # 分布をバープロットで表示（色を付ける）
-    barplot(distribution, col = rainbow(length(distribution)))
-    # グラフの保存を終了
-    dev.off()
-}
-
-
-
-get_structure <- function() {
-    jyuki_list <- .get_file_list("data", "^jyuki_.*\\.csv$")
-    config_header <- .get_config_headers()
-    .check_header(jyuki_list, config_header)
-    for (file in jyuki_list) {
-    .save_plot(file, "生年月日")
-    .save_plot(file, "性別")
-    .save_plot(file, "続柄")
+    # If none of the encodings worked, guess the encoding
+    if (!result) {
+        guessed_encoding <- .guess_file_encofing(filename)
+        data <- read_csv(filename, locale = locale(encoding = guessed_encoding))
     }
 
+    # Get a random sample of 100 rows or less
+    if (nrow(data) > 100) {
+        data <- data[sample(nrow(data), 100), ]
+    }
+
+    # Prepare the filename for saving
+    filename_saved <- paste0("output/random/", sub("\\.csv$", "", basename(filename)), "_random.csv")
+
+    # Check if the file already exists
+    if (file.exists(filename_saved)) {
+        # If the file exists, read it
+        existing_data <- read_csv(filename_saved)
+        # Add the new column to the existing data
+        existing_data[[header]] <- data[[header]]
+        # Write the updated data back to the file
+        write_csv(existing_data, file = filename_saved)
+    } else {
+        # If the file doesn't exist, create it with original header name
+        new_data <- data.frame(data[[header]])
+        names(new_data) <- header
+        write_csv(new_data, file = filename_saved)
+    }
+}
+  
+
+
+main <- function() {
+  setting <- .get_setting()
+  walk(names(setting), function(name) {
+    column <- setting[[name]]
+    column <- column[!is.na(column) & column != ""]
+    file_list <- .get_files(name)
+    walk(column, function(header) {
+      walk(file_list, function(file) {
+        .save_structure(file, header)
+        .save_random(file, header)
+      })
+    })
+  })
 }
 
-get_structure()
+main()
