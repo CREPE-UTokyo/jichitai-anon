d8a1a5dfc22ee0ed54a7967cf7a6c6cf6f0e2dcd
watagori <kotaro.nishigori@gmail.com>
2023-07-22 02:26:04 +0900

    feat: get random csv data

--- src/structure.r
+++ src/structure.r
@@ -1,5 +1,4 @@
-.libPaths(c("/my_packages", .libPaths()))
-
+# パッケージをインストールする関数
 # install_packages <- function(package_name) {
 #   if (!require(package_name, character.only = TRUE)) {
 #     install.packages(package_name, lib = "/my_packages", repos = "http://cran.us.r-project.org")
@@ -8,15 +7,20 @@
 # }
 
 # # Check and install necessary packages
-# necessary_packages <- c("showtext")
+# necessary_packages <- c("purrr")
 
 # sapply(necessary_packages, install_packages)
+.libPaths(c("/my_packages", .libPaths()))
+library(readr)
+library(dplyr)
 library(showtext)
+library(purrr)
 showtext_auto()
 font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
 
+
 .get_setting <- function() {
-  data <- read_csv("config/setting.csv")
+  data <- read_csv("config/setting_plot.csv")
   column_list <- lapply(data, as.character)
   return(column_list)
 }
@@ -24,13 +28,6 @@ font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
 .get_files <- function(filename) {
   file_list <- list.files(path = "data", pattern = paste0("^", filename, ".*\\.csv$"), full.names = TRUE)
 }
-
-.check_encofing <- function() {
-
-
-
-}
-
 
 .save_structure <- function(file, header) {
     encoding_list <- c("UTF-8", "Shift-JIS")
@@ -43,7 +40,7 @@ font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
             # Proceed if no error
             distribution <- table(data[[header]])
             filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
-            filename_saved <- paste0("output/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
+            filename_saved <- paste0("output/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
             png(filename = filename_saved, family = "IPAex")  # ここでフォントを指定
             barplot(distribution, 
                     xlab = filename, 
@@ -71,7 +68,7 @@ font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
         # Proceed if no error
         distribution <- table(data[[header]])
         filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
-        filename_saved <- paste0("output/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
+        filename_saved <- paste0("output/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
         png(filename = filename_saved, family = "IPAex")  # ここでフォントを指定
         barplot(distribution, 
                 xlab = filename, 
@@ -92,21 +89,67 @@ font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
 }
 
 
-main <- function() {
-  setting = .get_setting()
-  for (i in seq_along(setting)) {
-    for (j in seq_along(setting[[i]])) {
-      # If the value is NA or an empty string, skip
-      if (is.na(setting[[i]][j]) || setting[[i]][j] == "") {
-        next
-      }
-      file_list = .get_files(names(setting)[i])
-      for (k in seq_along(file_list)) {
-        .save_structure(file_list[k], setting[[i]][j])
-      }
-    }
-  }
-}
+.save_random <- function(filename, header) {
+    # Check the file encoding and read the file
+    encoding_list <- c("UTF-8", "Shift-JIS")
+    for (encoding in encoding_list) {
+        result <- tryCatch({
+            # Try to read the file with the current encoding
+            data <- read_csv(filename, locale = locale(encoding = encoding))
+            TRUE  # return TRUE if everything went well
+        },
+        error = function(e) {
+            FALSE  # return FALSE if there was an error
+        })
 
+        # If there was no error, exit the loop
+        if (result) break
+    }
+    # If none of the encodings worked, guess the encoding
+    if (!result) {
+        guessed_encoding <- .guess_file_encofing(filename)
+        data <- read_csv(filename, locale = locale(encoding = guessed_encoding))
+    }
+
+    # Get a random sample of 100 rows or less
+    if (nrow(data) > 100) {
+        data <- data[sample(nrow(data), 100), ]
+    }
+
+    # Prepare the filename for saving
+    filename_saved <- paste0("output/random/", sub("\\.csv$", "", basename(filename)), "_random.csv")
+
+    # Check if the file already exists
+    if (file.exists(filename_saved)) {
+        # If the file exists, read it
+        existing_data <- read_csv(filename_saved)
+        # Add the new column to the existing data
+        existing_data[[header]] <- data[[header]]
+        # Write the updated data back to the file
+        write_csv(existing_data, file = filename_saved)
+    } else {
+        # If the file doesn't exist, create it with original header name
+        new_data <- data.frame(data[[header]])
+        names(new_data) <- header
+        write_csv(new_data, file = filename_saved)
+    }
+}
+  
+
+
+main <- function() {
+  setting <- .get_setting()
+  walk(names(setting), function(name) {
+    column <- setting[[name]]
+    column <- column[!is.na(column) & column != ""]
+    file_list <- .get_files(name)
+    walk(column, function(header) {
+      walk(file_list, function(file) {
+        .save_structure(file, header)
+        .save_random(file, header)
+      })
+    })
+  })
+}
 
 main()
