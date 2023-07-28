a742215a31273b2509109a6887d245e46553b360
watagori <kotaro.nishigori@gmail.com>
2023-07-28 00:22:52 +0000

    feat: utf8 only

--- src/get_structure.r
+++ src/get_structure.r
@@ -36,7 +36,7 @@ font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
 
 .guess_file_encoding <- function(filename) {
   guess <- guess_encoding(filename, n_max = 1000)
-  return(paste0(guess$encoding, ":", guess$confidence))
+  return(paste0(guess$encoding[1]))
 }
 
 
@@ -45,115 +45,53 @@ font_add("IPAex", "/workspaces/jichitai-anon/ipaexm/ipaexm.ttf")
 }
 
 .save_plot <- function(file, header) {
-  encoding_list <- c("UTF-8", "Shift-JIS")
-
-  for (encoding in encoding_list) {
-    print(paste0("Processing  with encoding ", encoding))
-
-    result <- tryCatch(
-      {
-        # Try to read the file with the current encoding
-        data <- read_csv(file, show_col_types = FALSE, locale = locale(encoding = encoding))
-
-        # Proceed if no error
-        distribution <- table(data[[header]], useNA = "ifany")  # Add 'useNA = "ifany"' to count NA frequencies
-        filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
-        filename_saved <- paste0("to_crepe/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
-        png(filename = filename_saved, family = "IPAex") # ここでフォントを指定
-        barplot(distribution,
-          xlab = filename,
-          ylab = "Frequency",
-          col = rainbow(length(distribution))
-        )
-        TRUE # return TRUE if everything went well
-      },
-      error = function(e) {
-        FALSE # return FALSE if there was an error
-      },
-      finally = {
-        # Make sure to close the device even if an error occurs
-        if (dev.cur() != 1) dev.off()
-      }
-    )
-
-    # If there was no error, exit the loop
-    if (result) break
-  }
-
-  # If none of the encodings worked, guess the encoding
-  if (!result) {
-    guessed_encoding <- .guess_file_encoding(file)
-
-    # Try to read the file with the guessed encoding
-    data <- read_csv(file, locale = locale(encoding = guessed_encoding), show_col_types = FALSE)
-
-    # Proceed if no error
-    distribution <- table(data[[header]], useNA = "ifany")  # Add 'useNA = "ifany"' to count NA frequencies
-    filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
-    filename_saved <- paste0("to_crepe/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
-    png(filename = filename_saved, family = "IPAex") # ここでフォントを指定
-    barplot(distribution,
-      xlab = filename,
-      ylab = "Frequency",
-      col = rainbow(length(distribution))
-    )
-    dev.off()
-  }
+  encoding <- "UTF-8"
+  print(paste0("Processing  with encoding ", encoding))
+  result <- tryCatch(
+    {
+      data <- read_csv(file, show_col_types = FALSE, locale = locale(encoding = encoding))
+      distribution <- table(data[[header]], useNA = "ifany")  # Add 'useNA = "ifany"' to count NA frequencies
+      filename <- paste0(sub("\\.csv$", "", basename(file)), "_", header, ".png")
+      filename_saved <- paste0("to_crepe/plot/", sub("\\.csv$", "", basename(file)), "_", header, ".png")
+      png(filename = filename_saved, family = "IPAex") # ここでフォントを指定
+      barplot(distribution,
+        xlab = filename,
+        ylab = "Frequency",
+        col = rainbow(length(distribution))
+      )
+      TRUE # return TRUE if everything went well
+    },
+    error = function(e) {
+      FALSE # return FALSE if there was an error
+    },
+    finally = {
+      # Make sure to close the device even if an error occurs
+      if (dev.cur() != 1) dev.off()
+    }
+  )
 }
 
 
 .create_random_sample <- function(input_file, num_samples = 1000) {
-  encoding_list <- c("UTF-8", "Shift-JIS")
-  for (encoding in encoding_list) {
-    result <- tryCatch(
-      {
-        # Try to read the file with the current encoding
-        df <- read_csv(input_file, locale = locale(encoding = encoding), show_col_types = FALSE)
-        # Create a list to store the samples
-        sample_list <- list()
-        
-        # Generate samples for each column and add them to the list
-        for (col in names(df)) {
-          sample_list[[col]] <- sample(df[[col]], num_samples, replace = TRUE)
-        }
-
-        # Convert the list to a data frame
-        random_sample_df <- data.frame(sample_list)
-        
-        output_file <- paste0("to_crepe/random/", sub("\\.csv$", "", basename(input_file)), "_random.csv")
-        write_csv(random_sample_df, output_file) 
-        TRUE # return TRUE if everything went well
-      },
-      error = function(e) {
-        FALSE # return FALSE if there was an error
+  encoding <- "UTF-8"
+  result <- tryCatch(
+    {
+      df <- read_csv(input_file, locale = locale(encoding = encoding), show_col_types = FALSE)
+      sample_list <- list()
+      for (col in names(df)) {
+        sample_list[[col]] <- sample(df[[col]], num_samples, replace = TRUE)
       }
-    )
-
-    # If there was no error, exit the loop
-    if (result) break
-  }
-
-  # If none of the encodings worked, guess the encoding
-  if (!result) {
-    guessed_encoding <- .guess_file_encoding(input_file)
-
-    df <- read_csv(input_file, locale = locale(encoding = guessed_encoding)) 
-    
-    # Create a list to store the samples
-    sample_list <- list()
-    
-    # Generate samples for each column and add them to the list
-    for (col in names(df)) {
-      sample_list[[col]] <- sample(df[[col]], num_samples, replace = TRUE)
+      random_sample_df <- data.frame(sample_list)
+      output_file <- paste0("to_crepe/random/", sub("\\.csv$", "", basename(input_file)), "_random.csv")
+      write_csv(random_sample_df, output_file) 
+      TRUE # return TRUE if everything went well
+    },
+    error = function(e) {
+      FALSE # return FALSE if there was an error
     }
-
-    # Convert the list to a data frame
-    random_sample_df <- data.frame(sample_list)
-    
-    output_file <- paste0("to_crepe/random/", sub("\\.csv$", "", basename(input_file)), "_random.csv")
-    write_csv(random_sample_df, output_file) 
-  }
+  )
 }
+
 
 
 main <- function() {
